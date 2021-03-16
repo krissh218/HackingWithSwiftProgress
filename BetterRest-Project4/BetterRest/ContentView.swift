0a09dc49e99d70c96b887b76a6a7e2cba57555e7
Mohan Krishna <mohankrishna.veeramachineni@dcsg.com>
2021-03-16 17:43:26 -0400

    -- Project--4

---
+++ BetterRest-Project4/BetterRest/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Mohan Krishna on 3/15/21.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("When do you want to wake up?").font(.headline)) {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                    .frame(height: 150)
+                    .clipped()
+                }
+
+                Section(header: Text("Desired amount of sleep").font(.headline)) {
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+
+                Section(header: Text("Daily coffee intake").font(.headline)) {
+                    Picker("Select number of cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) { cups in
+                            (cups == 1) ? Text("1 cup") : Text("\(cups) cups")
+                        }
+                    }
+                    .labelsHidden()
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(height: 100)
+                    .clipped()
+                }
+
+                Section(header: Text("Your ideal bedtime is...").font(.headline)) {
+                    Text(calculatedBedtime)
+                        .font(Font.title.monospacedDigit())
+                }
+            }
+            .navigationBarTitle("BetterRest")
+        }
+    }
+
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+
+    var calculatedBedtime: String {
+        let model = SleepCalculator()
+
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+
+            return formatter.string(from: sleepTime)
+        } catch {
+            return "Sorry, there was a problem calculating your bedtime."
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11 Pro")
+    }
+}
